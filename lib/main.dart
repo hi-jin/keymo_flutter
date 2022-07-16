73607e8e7a190bd249b2bae1fa1dd5ad0ef38188
hi-jin <crushed7@o.cnu.ac.kr>
2022-07-17 01:59:59 +0900

    add HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keymo_flutter/view/home/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.teal,
+        ),
+      ),
+      initialRoute: HomeView.id,
+      routes: {
+        HomeView.id: (context) => HomeView(),
+      },
+    );
   }
 }
---
+++ lib/view/home/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  static String id = 'HomeView';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
